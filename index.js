730dcc09d5af0576899e552f65199901038a845e
anjwsm <anjalikushwahawsm@gmail.com>
2024-01-05 00:54:58 +0530

    Adding Scores and Reset button

--- index.js
+++ index.js
@@ -1,22 +1,130 @@
-let X_TEXT = "X";
-let O_TEXT = "O" ; 
-let currentPlayer = X_TEXT ; 
+//Making variabkes
+let currentPlayer = "X";
+let nextPlayer = "O";
+let playerTurn = currentPlayer;
+let playerx = document.getElementById("player1");
+let playerO = document.getElementById("player2");
+let resetbtn = document.getElementById("resetbutton");
+let boxes = Array.from(document.querySelectorAll(".box"));
+let player1Score = 0, 
+    player2Score = 0;
 
-let boxes =Array.from(document.querySelectorAll(".box")) ;
+let moves = 0;
+let tieScore = 0 ;
+let tiebtn = document.getElementById("tie");
 
 
-let spaces = Array(9).fill(null) ;
+//Function to change player's turn
+const changePlayerTurn = () => {
+  playerTurn = playerTurn === currentPlayer ? nextPlayer : currentPlayer;
+};
 
-for (let i=0 ; i< boxes.length ; i++){
-    boxes[i].addEventListener("click" , (event)=>{
-        const id = event.target.id ;
+//Function to start game
 
-        if (!spaces[id]){
-            spaces[id] = currentPlayer;
-            event.target.innerText = currentPlayer;
-            currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT ;
+const startGame = () => {
+  boxes.forEach((box) => {
+
+    box.addEventListener("click", (e) => {
+      if (!isThereAnyWinner() && !box.getAttribute('value')) {
+        //mal bhar diya
+        e.target.textContent = playerTurn;
+        box.setAttribute('value',playerTurn);
+        moves += 1;
+        // check kaiya
+        if (isThereAnyWinner()) {
+          if (playerTurn == "X") {
+            player1Score += 1;
+            playerx.innerHTML = player1Score;
+          } else {
+            player2Score += 1;
+            playerO.innerHTML = player2Score;
+          }
         }
 
-    })
+        //playe change
+         if (moves == 9 && !isThereAnyWinner()) {
+            tieScore += 1 ;
+            tiebtn.innerHTML = tieScore ;
+           
+          console.log("tie");
+        }
+        
+            changePlayerTurn();
+        
+        
+      }
+    });
+  });
+};
+//
+
+const isThereAnyWinner = () => {
+  if (
+    boxes[0].getAttribute('value') === boxes[1].getAttribute('value') &&
+    boxes[1].getAttribute('value') === boxes[2].getAttribute('value') &&
+    boxes[0] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[3].getAttribute('value') === boxes[4].getAttribute('value') &&
+    boxes[4].getAttribute('value') === boxes[5].getAttribute('value') &&
+    boxes[4] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[6].getAttribute('value') === boxes[7].getAttribute('value') &&
+    boxes[7].getAttribute('value') === boxes[8].getAttribute('value') &&
+    boxes[7] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[0].getAttribute('value') === boxes[3].getAttribute('value') &&
+    boxes[3].getAttribute('value') === boxes[6].getAttribute('value') &&
+    boxes[3] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[1].getAttribute('value') === boxes[4].getAttribute('value') &&
+    boxes[4].getAttribute('value') === boxes[7].getAttribute('value') &&
+    boxes[4] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[2].getAttribute('value') === boxes[5].getAttribute('value') &&
+    boxes[5].getAttribute('value') === boxes[7].getAttribute('value') &&
+    boxes[5] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[0].getAttribute('value') === boxes[4].getAttribute('value') &&
+    boxes[4].getAttribute('value') === boxes[8].getAttribute('value') &&
+    boxes[0] .getAttribute('value')
+  ) {
+    return true;
+  } else if (
+    boxes[2].getAttribute('value') === boxes[4].getAttribute('value') &&
+    boxes[4].getAttribute('value') === boxes[6].getAttribute('value') &&
+    boxes[2] .getAttribute('value')
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+function reset() {
+
+
+  boxes.forEach((eachBox) => {
+    eachBox.innerHTML=''
+    eachBox.setAttribute('value',"") ;
+  });
+
+playerTurn = 'X' ;
+moves = 0 ;
+
 }
 
+resetbtn.addEventListener("click", reset);
+
+startGame();
